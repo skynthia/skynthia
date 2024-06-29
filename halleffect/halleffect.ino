d7ba0176dc76f7121c264244dba8e122aff2394f
Skynthia <skynthiamusic@gmail.com>
2024-06-29 12:38:57 -0700

    hall effect sensor + haptic feedback pulses

--- halleffect/halleffect.ino
+++ halleffect/halleffect.ino
@@ -1,29 +1,49 @@
-#define NUM_BTNS 4
+int dov[4] = {5, 4, 3, 2};
+int dov_count = 0;
 
-const int hallSensorPin = 2;
-const int btns[NUM_BTNS] = {3, 4, 5, 6};
+int pulse_clock = 0;
+int pulse_count = 0;
+long ms;
+int pulse_length = 300;
+bool motor_value = true;
+int motor_pin = 7;
 
 void setup() {
-  pinMode(hallSensorPin, INPUT);
-  for (int i = 0; i < NUM_BTNS; i++) {
-    pinMode(btns[i], INPUT_PULLUP);
-  }
   Serial.begin(9600);
+  pinMode(motor_pin, OUTPUT);
+  pinMode(13, OUTPUT); // use built in LED for debugging
+  digitalWrite(motor_pin, motor_value);
+  ms = millis();
 }
 
 void loop() {
-  int hallValue = digitalRead(hallSensorPin);
-  
-  Serial.print("Hall Sensor Value: ");
-  //Serial.println(hallValue);
-  
-  // Use 4 buttons to generate binary value
-  int bin = 0;
-  for (int i = 0; i < NUM_BTNS; i++) {
-    int btn = !digitalRead(btns[i]);
-    bin = bin | (btn << i);
+  int binary_value = 0;
+  dov_count = 0;
+  for (int i = 0; i < 4; i++) {
+    int reading = digitalRead(dov[i]);
+    binary_value = binary_value | (!reading << i);
+    if (!reading) {
+      dov_count++;
+    }
   }
-  Serial.println(bin);
 
-  delay(100); // Add a small delay to avoid rapid repeated detections
+  //Serial.println(binary_value);
+
+  pulse_count = dov_count * 2; // multiply by two because of on and then off
+  
+  pulse();
+  delay(10);
+}
+
+void pulse() {
+  if (millis() - ms >= pulse_length) {
+    if (pulse_clock < pulse_count) {
+      motor_value = !motor_value;
+      digitalWrite(13, motor_value); // blink the internal LED for now
+      digitalWrite(motor_pin, motor_value);
+      //Serial.println(motor_value);
+    }
+    pulse_clock = (pulse_clock + 1) % 8;
+    ms = millis();
+  }
 }
