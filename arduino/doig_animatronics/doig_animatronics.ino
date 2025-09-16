4d43ccfaeacfa04bd3f57fb2cf1e8daa3cf8b0fa
Skynthia <skynthiamusic@gmail.com>
2025-09-15 20:57:06 -0700

    Updated doig animatronics

--- arduino/doig_animatronics/doig_animatronics.ino
+++ arduino/doig_animatronics/doig_animatronics.ino
@@ -46,12 +46,15 @@ bool prev_endstop1_val[3] = {false, false, false};
 long endstop_trig[3];
 int endstop_timeout = 500;
 
-int motor_pins[3][2] = {
+/*int motor_pins[3][2] = {
   // speed, direction
   {3, 4},
   {5, 8},
   {6, 7}
-};
+};*/
+
+int spd_pins[3] = {3, 11, 5};
+int dir_pins[3] = {4, 12, 8};
 
 // variables for tracking current behavior of motors
 long motor_move_start[3] = { -1, -1, -1 };
@@ -60,18 +63,20 @@ int motor_speed[3] = { 0, 0, 0 };
 bool motor_dir[3] = { true, true, true };
 
 // options for speed
-int speeds[5] = { 0, 64, 128, 192, 255 };
+int speeds[4] = { 255, 192, 128, 64 };
 
 void setup() {
   Serial.begin(9600);
   reseedRandom( &reseedRandomSeed );
 
   for (int i = 0; i < 3; i++) {
-    pinMode(motor_pins[i][0], OUTPUT); // speed
-    pinMode(motor_pins[i][1], OUTPUT); // direction
-    digitalWrite(motor_pins[i][0], LOW);
+    pinMode(spd_pins[i], OUTPUT); // speed
+    pinMode(dir_pins[i], OUTPUT); // direction
+    digitalWrite(spd_pins[i], LOW);
 
     endstop_trig[i] = millis();
+
+    newMove(i, false);
   }
 
   pinMode(endstop_pins[0][0], INPUT_PULLUP);
@@ -90,10 +95,9 @@ void loop() {
     bool endstop1_val = i == 0 ? digitalRead(endstop_pins[i][1]) : analogRead(endstop_pins[i][1]) > 0;
     //if (i == 1) Serial.println(endstop0_val);
     if (((endstop0_val && !prev_endstop0_val[i])  || (endstop1_val && !prev_endstop1_val[i])) && (millis() - endstop_trig[i]) >= endstop_timeout) {
-      Serial.print("Stopping motor ");
-      Serial.println(i);
       newMove(i, true);
       endstop_trig[i] = millis();
+      //digitalWrite(spd_pins[i], 0); // immediately stop the motor
     }
 
     prev_endstop0_val[i] = endstop0_val;
@@ -105,17 +109,27 @@ void loop() {
 void newMove(int which, bool end_stop) {
   // if this was an end stop, we go the other direction next
   if (end_stop) {
+    Serial.println((String) "Motor " + which + " endstop reached; reversing direction");
     motor_dir[which] = !motor_dir[which];
-    //Serial.println("endstop reached; reversing direction");
+    motor_speed[which] = 255;
+    motor_move_dur[which] = 4000;
   }
   else {
     // randomly generate a direction
     motor_dir[which] = random(2);
+    
+    // speed: quantized
+    int spd = random(4);
+    motor_speed[which] = speeds[spd];
+  
+    // duration: longer for slower speeds
+    spd++;
+    int dur = (1000 * spd) + random(2000 * spd);
+    motor_move_dur[which] = dur;
   }
 
-  // speed: quantized
-  motor_speed[which] = speeds[random(6)];
-
-  // duration: longer for slower speeds
-  
+  motor_move_start[which] = millis();
+  Serial.println((String) which + ":\t" + motor_speed[which] + "\t" + motor_dir[which] + "\t" + motor_move_dur[which]);
+  digitalWrite(dir_pins[which], motor_dir[which]);
+  analogWrite(spd_pins[which], motor_speed[which]);
 }
